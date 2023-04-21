a801161eefdb23970e168762b375c33f45b00a4b
alexis-leclerc-cegep <chepo@chepo.com>
2023-04-21 14:35:46 -0400

    nouvelle version gradle et ajout bouton alerte

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -30,16 +32,29 @@ class MainActivity : AppCompatActivity(){
         MqttClientHelper(this)
     }
 
+    private var seuilCo2 : Int = 1000
+    private var seuilTVOC : Int = 60
 
     private val client = OkHttpClient()
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         val sharedPreferences : SharedPreferences= getSharedPreferences("sharedPrefs", MODE_PRIVATE)
         if(sharedPreferences.getString("token", null) == null){
             Log.w("AndroidRuntime", "Token null, starting login")
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+            //TODO:: check if token is still valid
         }
+
+        if(sharedPreferences.getString("seuilCO2", null) != null){
+            seuilCo2 = sharedPreferences.getString("seuilCO2", null)!!.toInt()
+        }
+
+        if(sharedPreferences.getString("seuilTVOC", null) != null){
+            seuilTVOC = sharedPreferences.getString("seuilTVOC", null)!!.toInt()
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
@@ -68,6 +83,15 @@ class MainActivity : AppCompatActivity(){
             }
             Snackbar.make(view, snackbarMsg, Snackbar.LENGTH_SHORT)
                 .setAction("Action", null).show()
+        }
+
+        btnAlerte.setOnTouchListener { v, event ->
+            if (event.action == android.view.MotionEvent.ACTION_DOWN) {
+                mqttClient.publish("alexis/lumiere", "1")
+            } else if (event.action == android.view.MotionEvent.ACTION_UP) {
+                mqttClient.publish("alexis/lumiere", "0")
+            }
+            true
         }
 
         Timer("CheckMqttConnection", false).schedule(3000) {
@@ -150,13 +174,33 @@ class MainActivity : AppCompatActivity(){
             override fun messageArrived(topic: String, mqttMessage: MqttMessage) {
                 Log.w("Debug", "Message received from host '$SOLACE_MQTT_HOST': $mqttMessage")
                 when (topic){
-                    "alexis/co2" -> textViewCO2.text = "$mqttMessage ppm"
-                    "alexis/tvoc" -> textViewTVOC.text = "$mqttMessage ppm"
+                    "alexis/co2" -> displayCO2(mqttMessage.toString())
+                    "alexis/tvoc" -> displayTVOC(mqttMessage.toString())
                 }
             }
 
             override fun deliveryComplete(iMqttDeliveryToken: IMqttDeliveryToken) {
                 Log.w("Debug", "Message published to host '$SOLACE_MQTT_HOST'")
+            }
+
+            fun displayCO2(co2: String){
+                textViewCO2.text = "$co2 ppm"
+                if(co2.toInt() > seuilCo2){
+                    textViewCO2.setTextColor(Color.RED)
+                }
+                else{
+                    textViewCO2.setTextColor(Color.GREEN)
+                }
+            }
+
+            fun displayTVOC(tvoc: String){
+                textViewTVOC.text = "$tvoc ppm"
+                if(tvoc.toInt() > seuilTVOC){
+                    textViewTVOC.setTextColor(Color.RED)
+                }
+                else{
+                    textViewTVOC.setTextColor(Color.GREEN)
+                }
             }
         })
     }
