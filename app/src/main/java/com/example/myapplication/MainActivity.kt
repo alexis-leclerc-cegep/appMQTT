24348cd74766b406bc47742e78a33ede2906bbe0
alexis-leclerc-cegep <chepo@chepo.com>
2023-04-21 08:41:10 -0400

    nouvelle chose

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.mqtt.MqttClientHelper
+import com.example.myapplication.ui.login.LoginActivity
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -32,7 +34,9 @@ class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        //setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         val API_URL: String = "http://172.16.6.112:6969"
